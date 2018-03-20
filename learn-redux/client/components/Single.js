043146914aa0eaa44fc73a9a96b1ef0c41a99f64
Tyler Mabey <mabeytyler@gmail.com>
2018-03-19 23:50:26 -0600

    add single photo component

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Photo from './Photo';
 
 const Single = React.createClass({
   render() {
+    // index of the post
+    const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+    const post = this.props.posts[i];
+    
     return (
       <div className="single-photo">
-        single
+        <Photo i={i} post={post} {...this.props} />
       </div>
     )
   }
