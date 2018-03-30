9cf66e32ffaf5fe42184ba2b8269ae04e601cf1d
Tyler Mabey <mabeytyler@gmail.com>
2018-03-29 23:33:52 -0600

    update comment and single components

--- learn-redux/client/components/Comments.js
+++ learn-redux/client/components/Comments.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
 const Comments = React.createClass({
+
+  renderComment(comment, i) {
+    return (
+      <div className="comment" key={i}>
+        <p>
+          <strong>{comment.user}</strong>
+        </p>
+      </div>
+    )
+  },
+
   render() {
     return (
-      <div className="comment">Comment goes here</div>
+      <div className="comment">
+        {this.props.postComments.map(this.renderComment)}
+      </div>
     )
   }
 });
